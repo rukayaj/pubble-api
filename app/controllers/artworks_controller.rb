8fcbfe020885c54061165835c3ad77b10a087991
rukayaj-prodigy <rukaya.johaadien@prodigyfinance.com>
2018-05-11 15:07:01 +0200

    Fetches copyright and thumbnail from DST api

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,4 +1,5 @@
 class ArtworksController < ApplicationController
+  include HTTParty
   before_action :set_book, only: [:index, :create, :show, :update, :destroy]
   before_action :set_artwork, only: [:show, :update, :destroy]
 
@@ -17,6 +18,22 @@ class ArtworksController < ApplicationController
   # POST /artworks
   def create
     @artwork = @book.artworks.new(artwork_params)
+    dst_url = 'http://www.dreamstime.com/api/'
+    headers = {'Content-Type' => 'application/json'}
+
+    # Auth token
+    body = {'APIRequests': {'Authenticate': {'Verb': 'Authenticate', 'ApplicationId': 'WP-Plugin v2.0'}}}
+    response = HTTParty.post(dst_url, body: body.to_json, headers: headers) 
+    auth_token = response['APIResponses']['Authenticate']['AuthToken']
+
+    # Get image object
+    body = {'APIRequests': {'GetPaidImage': {'Verb': 'GetPaidImage', 'ImageId': @artwork.source_id, 'AuthToken': auth_token}}}
+    response = HTTParty.post(dst_url, body: body.to_json, headers: headers) 
+    image_object = JSON.parse(response.body)['APIResponses']['GetPaidImage']['Image']
+    
+    # Populate artwork
+    @artwork.thumbnail_url = image_object['ThumbnailUrl']
+    @artwork.copyright = image_object['Author']
     byebug
     if @artwork.save
       render json: @artwork, status: :created, location: book_url(@artwork)
@@ -51,6 +68,7 @@ class ArtworksController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def artwork_params
-      params.require(:artwork).permit(:book_id, :page, :topic, :source, :source_id, :copyright, :thumbnail_url, :notes)
+      params.require(:artwork).permit(:book_id, :page, :topic, :source, :source_id, :notes)
+      #params.require(:artwork).permit(:book_id, :page, :topic, :source, :source_id, :copyright, :thumbnail_url, :notes)
     end
 end
