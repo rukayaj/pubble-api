2fb0f8d59cc4054306eaafe5b593af08412ab18b
rukayaj-prodigy <rukaya.johaadien@prodigyfinance.com>
2018-05-11 17:45:16 +0200

    Refactor some more

--- app/services/dreamstime_service.rb
+++ app/services/dreamstime_service.rb
@@ -11,21 +11,23 @@ class DreamstimeService
   end
 
   def populate_thumbnail_and_copyright(artwork)
-    body = {'APIRequests': {'GetPaidImage': {'Verb': 'GetPaidImage', 'ImageId': artwork.source_id, 'AuthToken': @@auth_token}}}
-    response = HTTParty.post(BASE_URI, body: body.to_json, headers: JSON_HEADERS) 
-    response_obj = JSON.parse(response.body)
-   
+    response_obj = get_dst_image(artwork.source_id)
+ 
     # If the auth token is stale, refresh it and re-run the query. TODO find a nicer way of doing this 
     if response_obj['Status'] == 'Error' && (response_obj['ErrorCode'] == 201 || response_obj['ErrorCode'] == 200)
       self.auth
-      body = {'APIRequests': {'GetPaidImage': {'Verb': 'GetPaidImage', 'ImageId': artwork.source_id, 'AuthToken': @@auth_token}}}
-      response = HTTParty.post(BASE_URI, body: body.to_json, headers: JSON_HEADERS) 
-      response_obj = JSON.parse(response.body)
+      response_obj = get_dst_image(artwork.source_id)
     end
 
-    image_object = JSON.parse(response.body)['APIResponses']['GetPaidImage']['Image']
+    image_object = response_obj['APIResponses']['GetPaidImage']['Image']
     artwork.thumbnail_url = image_object['ThumbnailUrl']
     artwork.copyright = image_object['Author']
     artwork
   end 
+  
+  def get_dst_image(dst_image_id)
+    body = {'APIRequests': {'GetPaidImage': {'Verb': 'GetPaidImage', 'ImageId': dst_image_id, 'AuthToken': @@auth_token}}}
+    response = HTTParty.post(BASE_URI, body: body.to_json, headers: JSON_HEADERS) 
+    JSON.parse(response.body)
+  end
 end
